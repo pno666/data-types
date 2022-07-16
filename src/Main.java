4c72ac2bc8c5630d5d3e4d1dc768dc0f61a6c385
Kaplan Anton <lvinov44@gmail.com>
2022-07-16 12:44:37 +0300

    Reg Rep

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int mili = 20;
+
+        int ticketM = ticketPrice / mili;
+
+        System.out.println("Начисленные мили за покупку: " + ticketM);
+    }
+}
